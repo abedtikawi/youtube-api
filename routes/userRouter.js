453cccd0a1fffd20b32a0116dd1de384947eeace
Dano <kastaramation@gmail.com>
2021-06-15 23:03:36 +0300

    created router login user

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
 router.route('/register').post(require('../middlewares/users/validateCreateUser'),require('../controllers/users/createUser'))
-
+router.route('/login').post(require('../controllers/users/loginUser'))
 
 module.exports = router;

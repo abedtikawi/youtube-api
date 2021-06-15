fe69e6cd611d9ff295e92c9fed9543a0dc3666fb
Dano <kastaramation@gmail.com>
2021-06-16 00:03:35 +0300

    created /me in user router

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 
 router.route('/register').post(require('../middlewares/users/validateCreateUser'),require('../controllers/users/createUser'))
 router.route('/login').post(require('../middlewares/users/validateLoginUser'),require('../controllers/users/loginUser'))
+router.route('/me').get(require('../controllers/users/findUser'))
 
 module.exports = router;

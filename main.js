dbbf1971c46278b292fc08559a908bddb980101d
Dano <kastaramation@gmail.com>
2021-06-15 21:18:48 +0300

    created folder structure

--- main.js
+++ main.js
@@ -5,17 +5,20 @@ require("dotenv").config();
 const express = require("express");
 
 // load files
-const connectDB=require('./db/dbController');
-
+const connectDB = require("./db/dbController");
+const indexRouter = require("./routes/router");
+// create app instance of express
 const app = express();
 
 // bind app to parse json
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// connect to DB
-connectDB(); 
+// bind app instance to router
+app.use("/api/", indexRouter);
 
+// connect to DB
+connectDB();
 
 app.listen(process.env.PORT, () => {
   console.log(`Running on Port ${process.env.PORT}`);

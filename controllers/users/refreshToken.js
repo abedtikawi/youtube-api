3140ab714345bd92459c4ed1ff13267c5d3bbd5c
AbedTikkawi <37665841+abedtikawi@users.noreply.github.com>
2021-06-16 16:25:58 +0300

    fixed github credential manager issue, created refresh token controller

---
+++ controllers/users/refreshToken.js
@@ -0,0 +1,11 @@
+const jwt = require('jsonwebtoken');
+const Users = require('../../models/users');
+module.exports = async (req, res) => {
+  try {
+    return res.status(200).json({ message: 'Success' });
+  } catch (error) {
+    console.log('-- Error in RefreshToken.js');
+    console.log(error);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+};

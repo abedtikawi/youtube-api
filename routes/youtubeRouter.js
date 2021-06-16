9b61038f4eac45a74f469ce831d92817147b8d0a
Dano <kastaramation@gmail.com>
2021-06-16 09:38:24 +0300

    created userVideo controller with route /

--- routes/youtubeRouter.js
+++ routes/youtubeRouter.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-
-
+const isUser = require("../middlewares/users/validateJWT");
+router.route("/").get(require("../controllers/youtube/userVideos"));
 
 module.exports = router;

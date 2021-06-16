6474c0698d484330f16b64faee134b2c79b7c64a
Malekh198 <71446225+Malekh198@users.noreply.github.com>
2021-06-16 15:10:42 +0300

    created videoDetails.js with route

--- routes/youtubeRouter.js
+++ routes/youtubeRouter.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
 const isUser = require("../middlewares/users/validateJWT");
 router.route("/").get(isUser, require("../controllers/youtube/userVideos"));
-
+router.route("/:id").get(isUser,require("../controllers/youtube/videoDetails"))
 module.exports = router;

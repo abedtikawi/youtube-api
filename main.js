a0673d174412d47ba45dc8ca79a9edd7c03bdd2d
Dano <kastaramation@gmail.com>
2021-06-16 22:12:22 +0300

    attached fb router to app instance

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 // environment variables
-require('dotenv').config();
+require("dotenv").config();
 
 // load libraries
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 // load files and routers
-const connectDB = require('./db/dbController');
-const indexRouter = require('./routes/userRouter');
-const youtubeRouter = require('./routes/youtubeRouter');
+const connectDB = require("./db/dbController");
+const indexRouter = require("./routes/userRouter");
+const youtubeRouter = require("./routes/youtubeRouter");
+const facebookRouter = require("./routes/facebookRouter");
 // create app instance of express
 const app = express();
 
@@ -18,8 +19,9 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 // bind app instance to router
-app.use('/users/', indexRouter);
-app.use('/youtube/', youtubeRouter);
+app.use("/users/", indexRouter);
+app.use("/youtube/", youtubeRouter);
+app.use("/facebook/", facebookRouter);
 
 // connect to DB
 connectDB();

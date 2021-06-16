acfa4a9324936e6025fe6c0f8507385b1fb0c5c4
Dano <kastaramation@gmail.com>
2021-06-16 12:02:56 +0300

    modified youtube/ route

--- controllers/youtube/userVideos.js
+++ controllers/youtube/userVideos.js
@@ -3,25 +3,61 @@ const axios = require("axios");
 module.exports = async (req, res) => {
   try {
     const findUser = await Users.findById(req.user.id);
-    if (!findUser) {
-      return res.status(400).json({ message: "User does not exist" });
-    }
-    const queryOptions = {
+    const headers = {
+      Accept: "application/json",
+    };
+    const channelQuery = {
       part: "snippet,contentDetails,statistics",
       id: `${findUser.youtube_channel_id}`,
       key: `${process.env.CLIENT_API_KEY}`,
     };
-    const headers = {
-      Accept: "application/json",
-    };
-    const response = await axios.get(
+
+    const getChannel = await axios.get(
       `https://youtube.googleapis.com/youtube/v3/channels`,
-      { params: queryOptions },
+      { params: channelQuery },
       headers
     );
+    if (getChannel.data.pageInfo.totalResults === 0) {
+      return res
+        .status(400)
+        .json({ message: "This channel has no videos on Youtube " });
+    }
+    const channel = getChannel.data.items[0];
+    const playlistId = channel.contentDetails.relatedPlaylists.uploads;
 
+    if (!findUser) {
+      return res.status(400).json({ message: "User does not exist" });
+    }
 
-    return res.status(200).json({ message: "Success", api: response.data });
+    const playlistQuery = {
+      part: "snippet",
+      playlistId: playlistId,
+      key: `${process.env.CLIENT_API_KEY}`,
+    };
+
+    const getPlaylists = await axios.get(
+      `https://youtube.googleapis.com/youtube/v3/playlistItems`,
+      { params: playlistQuery },
+      headers
+    );
+    const videos = getPlaylists.data.items;
+    if (!videos) {
+      return res
+        .status(400)
+        .json({ message: "This Youtube Channel does not have videos" });
+    }
+    let youtubeVideos = [];
+    videos.forEach((element) => {
+      let obj = {
+        videoURL: `https://www.youtube.com/watch?v=${element.snippet.resourceId.videoId}`,
+      };
+      youtubeVideos.push(obj);
+    });
+
+    return res.status(200).json({
+      message: "Success",
+      api: youtubeVideos,
+    });
   } catch (error) {
     console.log("-- Error in userVideos.js");
     console.log(error);

b72fa90a0fe14fb66e3b2a8cc632addcb581beed
Dano <kastaramation@gmail.com>
2021-06-17 11:08:28 +0300

    changed comments in getUserDetails.js

--- controllers/facebook/getUserDetails.js
+++ controllers/facebook/getUserDetails.js
@@ -1,6 +1,10 @@
 const axios = require("axios");
 module.exports = async (req, res) => {
   try {
+    // get the user id from user's jwt
+    // get facebookID of the  user from the database
+    // authenticate the user to get access token
+    // then perform the axios request with the user's token to recieve the params.fields values
     const paramOptions = {
       fields: "id,name,email,gender,picture,likes",
       // there must be a login screen to authenticate the user using OAUTH and then the access token is the user's token
@@ -10,7 +14,7 @@ module.exports = async (req, res) => {
     const response = await axios.get(`https://graph.facebook.com/v11.0/me`, {
       params: paramOptions,
     });
- 
+
     return res.status(200).json({ message: "Success", api: response.data });
   } catch (error) {
     console.log("-- Error in getUserDetails.js");

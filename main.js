81304742d8f352d52f37d5b2c163521d4dfdffe4
Dano <kastaramation@gmail.com>
2021-06-17 11:13:17 +0300

    added comments to main.js

--- main.js
+++ main.js
@@ -5,26 +5,32 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+
 // load files and routers
 const connectDB = require("./db/dbController");
 const indexRouter = require("./routes/userRouter");
 const youtubeRouter = require("./routes/youtubeRouter");
 const facebookRouter = require("./routes/facebookRouter");
+
+// connect to DB
+connectDB();
 // create app instance of express
 const app = express();
 
 // bind app to parse json
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// bind app to cors library for cross origin resource sharing
 app.use(cors());
+
+// bind app to use cookies
 app.use(cookieParser());
+
 // bind app instance to router
 app.use("/users/", indexRouter);
 app.use("/youtube/", youtubeRouter);
 app.use("/facebook/", facebookRouter);
-
-// connect to DB
-connectDB();
 
 app.listen(process.env.PORT, () => {
   console.log(`Running on Port ${process.env.PORT}`);
